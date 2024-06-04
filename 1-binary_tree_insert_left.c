86596150d5c0cf697fe57391049624b259721c96
Yermehyaw <yermehyaw@gmail.com>
2024-06-04 11:53:34 +0100

    Add edge case

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 	binary_tree_t *temp;
 
+	if (parent == NULL)
+		return (NULL);
 	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
