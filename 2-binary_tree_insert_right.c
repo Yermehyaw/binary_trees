054b750a827207fcf25bc13e3eea61e05539c10a
Yermehyaw <yermehyaw@gmail.com>
2024-06-04 12:02:41 +0100

    Remove stray ;

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,7 +8,7 @@
  * Return: Memory location of new node
  */
 
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 	binary_tree_t *temp;
