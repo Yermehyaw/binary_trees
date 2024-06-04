ac228a4af2a5b475f834e11cff052c2ed5f04592
Yermehyaw <yermehyaw@gmail.com>
2024-06-04 03:25:20 -0700

    Fix betty style err

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,10 +17,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
-		return NULL;
+		return (NULL);
 	node->parent = parent;
 	node->n = value;
 	node->left = NULL;
 	node->right = NULL;
-	return node;
+	return (node);
 }
