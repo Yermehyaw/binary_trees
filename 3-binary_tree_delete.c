aa3ec9fd82f566094e1b9ae726da816e697bbdac
Yermehyaw <yermehyaw@gmail.com>
2024-06-05 22:24:11 +0100

    Delete a BT

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+void free_subtree(binary_tree_t *subtree);
+
 /**
  * binary_tree_delete - Deletes a binary tree
  * @tree: binary tree
@@ -7,17 +9,32 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *temp_left;
-	binary_tree_t *temp_right;
+	binary_tree_t *subtree_left;
+	binary_tree_t *subtree_right;
 
-	if (tree == NULL)
-		return (NULL);
-	temp_left = tree->left;
-	temp_right = tree->right;
-	while (temp_left)
+	subtree_left = tree->left;
+	subtree_right = tree->right;
+	free_subtree(subtree_left); /* recursively free left subtree */
+	free_subtree(subtree_right); /* recursively free right subtree */
+	free(tree);
+}
+
+
+/**
+ * free_subtree - Recursively frees a binary tree
+ * @subtree: root node or subtree
+ */
+
+void free_subtree(binary_tree_t *subtree)
+{
+	if (subtree != NULL)
 	{
-		free(temp_left)
-		temp_left = tree->left;
-		tree->left = tree->left->left;
+		/* call recursively to all left and right nodes */
+		free_subtree(subtree->left);
+		free_subtree(subtree->right);
+		/* Free all left and right nodes recursively */
+		/*free(subtree->left);
+		  free(subtree->right);*/
 	}
+	free(subtree);
 }
